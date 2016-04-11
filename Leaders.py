8a945a8e17a4ccc0c0c4baaba3057811824d851c
Ryan Coady <rcoady91@gmail.com>
2016-04-11 18:31:20 -0400

    Adds Leaders.py file and adds leader test data from Sorter.py

---
+++ Leaders.py
@@ -0,0 +1,17 @@
+import csv
+import math
+import random
+import sys
+# import Sorter
+
+leaderInfo = []
+
+leaderArray = [['Ryan Coady', '1'], ['Amber Dalgaard', '2'], ['Hopper', '2'], ['Kristin', '3'], ['Jon', '4'],
+               ['Jon', '4'], ['Leah', '5'], ['Jenna', '5'], ['Annalisa', '7']]
+
+#with open('Leaders.csv', 'rb') as f:
+#    reader = csv.reader(f)
+#    leaderArray = list(reader)
+
+
+print leaderArray
