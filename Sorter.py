195968733c96f5c0bae5bf4268d7058e356a8dd1
Ryan Coady <rcoady91@gmail.com>
2016-04-17 18:10:47 -0400

    Refactors code into functions filtername, donoraddress, and calculatethankyouamount

--- Sorter.py
+++ Sorter.py
@@ -12,36 +12,46 @@ with open('testDonors2013.csv', 'rb') as f:
 donationLevel = 250
 addressArray = []
 fullDonorArray = []
-multipleDonor = []
-
-for donor in donorArray:
-    # Checks for the contributor and contact being the same person
-    if donor[0] == donor[1]:
-        donor[1] = ''
-    if donor[2] == '':
-        donor[2] = 0
-
-    # Writes donor name, contact, and address to an array
-    if donor[4] != "Staff":
-        addressArray.append([donor[0], donor[1], donor[5], donor[6], donor[7], donor[8]])
-
-    # How many thank you's a person should receive
-    donation = float(donor[2])
-    amount = int(math.ceil(donation / donationLevel))
-
-    # Catches edge case if the donation is zero or blank
-    if amount == 0:
-        amount = 1
-
-    # Array for Multiple thank yous
-    if amount > 1:
-        multipleDonor.append([donor[0], donor[1]])
-
-    while amount > 0:
-        amount -= 1
-        fullDonorArray.append([donor[0], donor[1], donor[3], amount + 1])
+cleanedArray = []
 
 
+def filtername(donorfile):
+    for donor in donorfile:
+        if donor[0] == donor[1]:
+            donor[1] = ''
+        if donor[2] == '':
+            donor[2] = 0
+        cleanedArray.append([donor[0], donor[1], donor[2], donor[3], donor[4], donor[5], donor[6], donor[7], donor[8]])
+    return cleanedArray
+
+
+def donoraddress(donorfile):
+    for donor in donorfile:
+        if donor[4] != "Staff":
+            addressArray.append([donor[0], donor[1], donor[5], donor[6], donor[7], donor[8]])
+
+    with open("Address Labels.csv", "wb") as f:
+        address_writer = csv.writer(f)
+        address_writer.writerow(["Company", "Contact", "Address", "City", "State", "Zip"])
+        address_writer.writerows(addressArray)
+
+
+def calculatethankyouamount(donorfile):
+    for donor in donorfile:
+        donation = float(donor[2])
+        amount = int(math.ceil(donation / donationLevel))
+
+        if amount == 0:
+            amount = 1
+        while amount > 0:
+            amount -= 1
+            fullDonorArray.append([donor[0], donor[1], donor[3], amount + 1])
+    return fullDonorArray
+
+
+filtername(donorArray)
+donoraddress(cleanedArray)
+calculatethankyouamount(cleanedArray)
 
 # Writes everything in the fullDonorArray to a csv file
 with open("Full Donor.csv", "wb") as f:
@@ -49,10 +59,6 @@ with open("Full Donor.csv", "wb") as f:
     writer.writerow(["Company", "Contact", "Reason for Thanking", "Count"])
     writer.writerows(fullDonorArray)
 
-with open("Address Labels.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Company", "Contact", "Address", "City", "State", "Zip"])
-    writer.writerows(addressArray)
 
 # TODO Read in csv for leaders
 # TODO Assign leaders to donors
