5d0d653827fe50a64b800f9304c8fa89b534c762
Ryan Coady <rcoady91@gmail.com>
2016-04-17 23:46:09 -0400

    Reads in leader file, cleans names of extra spaces, and adds all names to an array until there are at least as many leaders as donors

--- Leaders.py
+++ Leaders.py
@@ -2,16 +2,44 @@ import csv
 import math
 import random
 import sys
+import sqlite3
 # import Sorter
 
-leaderInfo = []
-
-leaderArray = [['Ryan Coady', '1'], ['Amber Dalgaard', '2'], ['Hopper', '2'], ['Kristin', '3'], ['Jon', '4'],
-               ['Jon', '4'], ['Leah', '5'], ['Jenna', '5'], ['Annalisa', '7']]
-
-#with open('Leaders.csv', 'rb') as f:
-#    reader = csv.reader(f)
-#    leaderArray = list(reader)
+fullLeaderArray = []
 
 
-print leaderArray
+def namecleaner():
+    for leader in leaderArray:
+        leader[0] = leader[0].strip()
+        leader[1] = leader[1].strip()
+
+
+def needaname():
+    total = 0
+    # 300 will be changed after reading in the length of the donor array
+    amount = int(math.ceil(300 / float(len(leaderArray))))
+    while total < amount:
+        for leader in leaderArray:
+            fullLeaderArray.append([leader[0], leader[1], leader[2]])
+        total += 1
+
+    # Shuffles the leaders names
+    random.shuffle(fullLeaderArray)
+
+
+# Opens Leader file and reads it into an array
+with open('testLeaders2013.csv', 'rb') as f:
+    reader = csv.reader(f)
+    leaderArray = list(reader)
+
+# Strips extra spaces from the leader first and last name
+namecleaner()
+
+# Writes the leaders to fullLeaderArray while the amount has not been fulfilled
+needaname()
+
+# Writes everything in the fullDonorArray to a csv file
+with open("Leaders.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Name", "Last Name", "Group"])
+    writer.writerows(fullLeaderArray)
