cd16abbebac2a6dccc92ba9c60bf047989058884
Ryan Coady <rcoady91@gmail.com>
2016-04-19 22:20:40 -0400

    Adds ability to combine both leader and donor arrays and write them to a singular csv file

--- Leaders.py
+++ Leaders.py
@@ -14,10 +14,10 @@ def namecleaner():
         leader[1] = leader[1].strip()
 
 
-def needaname():
+def needaname(length):
     total = 0
     # 300 will be changed after reading in the length of the donor array
-    amount = int(math.ceil(300 / float(len(leaderArray))))
+    amount = int(math.ceil(length / float(len(leaderArray))))
     while total < amount:
         for leader in leaderArray:
             fullLeaderArray.append([leader[0], leader[1], leader[2]])
@@ -27,19 +27,22 @@ def needaname():
     random.shuffle(fullLeaderArray)
 
 
+def write_leader_file(leader_array):
+    with open("Leaders.csv", "wb") as f:
+        writer = csv.writer(f)
+        writer.writerow(["FirstName", "Last Name", "Group"])
+        writer.writerows(leader_array)
+
 # Opens Leader file and reads it into an array
 with open('testLeaders2013.csv', 'rb') as f:
     reader = csv.reader(f)
     leaderArray = list(reader)
 
 # Strips extra spaces from the leader first and last name
-namecleaner()
+#namecleaner()
 
 # Writes the leaders to fullLeaderArray while the amount has not been fulfilled
-needaname()
+#needaname(246)
 
 # Writes everything in the fullDonorArray to a csv file
-with open("Leaders.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Name", "Last Name", "Group"])
-    writer.writerows(fullLeaderArray)
+#write_leader_file(fullLeaderArray)
--- Sorter.py
+++ Sorter.py
@@ -2,6 +2,7 @@ import csv
 import math
 import random
 import sys
+import Leaders
 
 
 # Opens Donor file and reads it into an array
@@ -53,12 +54,34 @@ filtername(donorArray)
 donoraddress(cleanedArray)
 calculatethankyouamount(cleanedArray)
 
+
+
 # Writes everything in the fullDonorArray to a csv file
 with open("Full Donor.csv", "wb") as f:
     writer = csv.writer(f)
     writer.writerow(["Company", "Contact", "Reason for Thanking", "Count"])
     writer.writerows(fullDonorArray)
+print "The full donor list has been made"
 
 
-# TODO Read in csv for leaders
+with open('testLeaders2013.csv', 'rb') as f:
+    reader = csv.reader(f)
+    leaderArray = list(reader)
+Leaders.namecleaner()
+Leaders.needaname(len(fullDonorArray))
+Leaders.write_leader_file(Leaders.fullLeaderArray)
+
+new_array = []
+# Stops after last assigned thank you
+print len(fullDonorArray)
+count = 0
+for donor in fullDonorArray:
+    new_array.append(fullDonorArray[count] + Leaders.fullLeaderArray[count])
+    count += 1
+
+with open("Full List.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Company", "Contact", "Reason for Thanking", "Count", "Leader First", "Leader Last", "Group"])
+    writer.writerows(new_array)
 # TODO Assign leaders to donors
+# TODO Clean donor file of extra spaces
