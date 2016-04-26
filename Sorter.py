ac4a6b1db434f290fba09ef3e21e780f0a3a4b38
Ryan Coady <rcoady91@gmail.com>
2016-04-25 21:53:25 -0400

    Adds method to write files and replaces write methods with calls to write method

--- Sorter.py
+++ Sorter.py
@@ -2,7 +2,6 @@ import csv
 import Leaders
 import donors
 
-
 # Opens Donor file and reads it into an array
 with open('testDonors2013.csv', 'rb') as f:
     reader = csv.reader(f)
@@ -13,17 +12,21 @@ donors.donoraddress(donors.cleanedArray)
 donors.calculatethankyouamount(donors.cleanedArray)
 
 
-# Writes everything in the fullDonorArray to a csv file
-with open("Full Donor.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Company", "Contact", "Reason for Thanking", "Count"])
-    writer.writerows(donors.fullDonorArray)
-print "The full donor list has been made"
+def writefile(filename, header, array):
+    with open(filename, "wb") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(array)
 
+
+writefile("Full Donor.csv", ["Company", "Contact", "Reason for Thanking", "Count"], donors.fullDonorArray)
+
+print "The full donor list has been made"
 
 with open('testLeaders2013.csv', 'rb') as f:
     reader = csv.reader(f)
     leaderArray = list(reader)
+
 Leaders.namecleaner()
 Leaders.needaname(len(donors.fullDonorArray))
 Leaders.write_leader_file(Leaders.fullLeaderArray)
@@ -36,9 +39,9 @@ for donor in donors.fullDonorArray:
     new_array.append(donors.fullDonorArray[count] + Leaders.fullLeaderArray[count])
     count += 1
 
-with open("Full List.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Company", "Contact", "Reason for Thanking", "Count", "Leader First", "Leader Last", "Group"])
-    writer.writerows(new_array)
+writefile("Full List.csv",
+          ["Company", "Contact", "Reason for Thanking", "Count", "Leader First", "Leader Last", "Group"],
+          new_array)
+
 # TODO Assign leaders to donors
 # TODO Clean donor file of extra spaces
