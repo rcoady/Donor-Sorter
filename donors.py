d28508878dd2acce638b24dfb732453a045eaaa1
Ryan Coady <rcoady91@gmail.com>
2016-04-25 23:03:46 -0400

    Adds lookup method to staff and multiple thank you's

--- donors.py
+++ donors.py
@@ -5,6 +5,8 @@ donationLevel = 250
 addressArray = []
 fullDonorArray = []
 cleanedArray = []
+test_array = []
+
 
 def filtername(donorfile):
     for donor in donorfile:
@@ -19,12 +21,25 @@ def filtername(donorfile):
 def donoraddress(donorfile):
     for donor in donorfile:
         if donor[4] != "Staff":
-            addressArray.append([donor[0], donor[1], donor[5], donor[6], donor[7], donor[8]])
+            addressArray.append([donor[0], donor[1], donor[5], donor[6], donor[7], donor[8], donor[9]])
 
     with open("Address Labels.csv", "wb") as f:
         address_writer = csv.writer(f)
-        address_writer.writerow(["Company", "Contact", "Address", "City", "State", "Zip"])
+        address_writer.writerow(["Company", "Contact", "Address", "City", "State", "Zip", "Lookup"])
         address_writer.writerows(addressArray)
+
+
+def lookup(donorfile):
+    for donor in donorfile:
+        if float(donor[2]) > donationLevel:
+            test_array.append([donor[0], donor[1], donor[2], donor[3], donor[4], donor[5], donor[6], donor[7], donor[8],
+                               "Multiple"])
+        if donor[4] == "Staff":
+            test_array.append(
+                    [donor[0], donor[1], donor[2], donor[3], donor[4], donor[5], donor[6], donor[7], donor[8], "Staff"])
+        elif float(donor[2]) <= donationLevel:
+            test_array.append(
+                    [donor[0], donor[1], donor[2], donor[3], donor[4], donor[5], donor[6], donor[7], donor[8], ''])
 
 
 def calculatethankyouamount(donorfile):
@@ -36,5 +51,5 @@ def calculatethankyouamount(donorfile):
             amount = 1
         while amount > 0:
             amount -= 1
-            fullDonorArray.append([donor[0], donor[1], donor[3], amount + 1])
+            fullDonorArray.append([donor[0], donor[1], donor[3], amount + 1, donor[9]])
     return fullDonorArray
